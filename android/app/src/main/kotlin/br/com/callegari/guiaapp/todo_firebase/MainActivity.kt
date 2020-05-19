ee31c6f5254f62d017a95069782ae00f7ad12b82
guilherme <xcallegarix.99@gmail.com>
2020-05-19 00:13:04 -0300

    to_do_list

---
+++ android/app/src/main/kotlin/br/com/callegari/guiaapp/todo_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.callegari.todo_firebase.todo_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/home/models/todo_model.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class TodoModel {
+  String title;
+  bool check;
+  DocumentReference reference;
+
+  TodoModel({
+    this.reference,
+    this.title = '',
+    this.check = false,
+  });
+
+  factory TodoModel.fromDocument(DocumentSnapshot doc) {
+    return TodoModel(
+        title: doc['title'], check: doc['check'], reference: doc.reference);
+  }
+
+  Future save() async {
+    if (reference == null) {
+      //adicionar um novo item
+      int total = (await Firestore.instance.collection('todo').getDocuments())
+          .documents
+          .length;
+      reference = await Firestore.instance
+          .collection('todo')
+          .add({'title': title, 'check': check, 'position': total});
+    } else {
+      reference.updateData({'title': title, 'check': check});
+    }
+  }
+
+ Future delete(){
+    return reference.delete(); 
+  }
+}
